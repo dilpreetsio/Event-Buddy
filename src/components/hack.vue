6a2acc02332189f1c39e499ac46faf382746a3b9
Dilpreet Singh <singhsince94@gmail.com>
2017-09-22 03:29:38 +0530

    feat: Adds listing using component

--- src/components/hack.vue
+++ src/components/hack.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="root ui card">
-    {{hack.name}}
+  <div class="ui segment">
+    <a :href=hack.website>
+      <div class="ui grid">
+        <div class="ui row">
+          <div class="ui column four wide">
+            <img class="ui tiny circular image fluid" src="http://photos.prnewswire.com/prnvar/20150915/266966LOGO">
+          </div>
+          <div class="ui column twelve wide data">
+            <h3 class="ui header">
+              {{hack.name}}
+            </h3>
+            <div class="time">
+              {{hack.start}} - {{hack.end}}
+            </div>
+            <div class="location">
+              <h5 class="ui header">
+                {{hack.location}}
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 
@@ -11,6 +32,12 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .location {
+    padding: 5px
+  }
+  .data {
+    text-align: right;
+    color: #000000;
+  }
 </style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -30,23 +30,8 @@ export default {
   }
 }
 </script>
-
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .header.ui.row {
+    text-align: center;
+  }
 </style>
