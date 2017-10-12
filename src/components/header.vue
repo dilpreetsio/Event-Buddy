70d69247e58736004ec762dc05def98afc56df73
chaintng <chaintng@gmail.com>
2017-10-12 21:56:47 +0700

    ISSUE-18 add month using moment to homepage

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="header ui row">
     <img class="logo" src="../assets/logo.png">
+    <div class="month ui row">{{month}}</div>
   </div>
 </template>
 <script>
+import moment from 'moment'
+
 export default {
+  data () {
+    return {
+      month: moment().format('MMMM YYYY')
+    }
+  }
 }
 </script>
 <style scoped>
-.header.ui.row {
+.ui.row {
   text-align: center;
-  color: white !important;
   margin-bottom: 0 !important;
+  padding: 30px 0;
+}
+
+.header {
   padding: 30px 0;
 }
 
@@ -19,4 +30,9 @@ export default {
   height: 50px;
   width: auto;
 }
+
+.month {
+  font-size: 1.4em;
+}
+
 </style>
