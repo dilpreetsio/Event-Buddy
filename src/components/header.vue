2a028e5f4d3f37a80ea0d2b8d120e8efe9c08185
George Mickleburgh <g.mickleburgh@gmail.com>
2017-10-12 13:06:13 +0100

    Changes to header and favicon (#20)
    * Added logo to header, changed header colour to white and added favicon.
    Closes #19
    
    * Removed commented out header text
    
    * Removed background colour declaration from the header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header ui row">
-    <h1>Event Buddy</h1>
+    <img class="logo" src="../assets/logo.png">
   </div>
 </template>
 <script>
@@ -10,8 +10,13 @@ export default {
 <style scoped>
 .header.ui.row {
   text-align: center;
-  background-color: #0980fb !important;
   color: white !important;
   margin-bottom: 0 !important;
+  padding: 30px 0;
+}
+
+.header .logo {
+  height: 50px;
+  width: auto;
 }
 </style>
