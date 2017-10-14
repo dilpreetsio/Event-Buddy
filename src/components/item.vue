94c1c890296d497f1bc9b4bd0310458dba2c6d8b
khushmeen <khushmeensidhu@gmail.com>
2017-10-15 02:58:51 +0530

    Added default image for events

--- src/components/item.vue
+++ src/components/item.vue
@@ -5,7 +5,7 @@
         <div class="ui row">
           <div class="ui column four wide">
             <img v-if="event.logo" class="ui tiny circular image fluid" v-bind:src="event.logo">
-            <img v-else class="ui tiny circular image fluid" src="http://photos.prnewswire.com/prnvar/20150915/266966LOGO">
+            <img v-else class="ui tiny circular image fluid" src="../assets/icon.png">
           </div>
           <div class="ui column twelve wide data">
             <h3 class="ui header">
