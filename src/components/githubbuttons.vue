adb9fbcebb15aedae788a51869b9b97a837ad04c
Bruno Orlandi <brorlandi@gmail.com>
2017-10-15 04:41:17 -0200

    Another implementation of GitHub buttons (#38)
    * GitHub Buttons
    
    * Reimplment Github Buttons with ntkme/github-buttons

---
+++ src/components/githubbuttons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="gh-buttons">
+    <a class="github-button" href="https://github.com/geekyd/Event-Buddy" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star geekyd/Event-Buddy on GitHub">Star</a>
+    <a class="github-button" href="https://github.com/geekyd/Event-Buddy/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork geekyd/Event-Buddy on GitHub">Fork</a>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+.gh-buttons{
+  margin-top: 15px;
+}
+</style>
